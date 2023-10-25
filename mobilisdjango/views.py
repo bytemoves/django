e79d8abbb4867b8da6c1f9e912d7fe9896249911
bytemoves <morganmogean@gmail.com>
2023-10-26 00:28:19 +0300

    django commits

---
+++ mobilisdjango/views.py
@@ -0,0 +1,8 @@
+#all logic
+from django.shortcuts import render
+def home(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request, "about.html")
+def gallery(request):
+    return render(request,"gallery.html")
